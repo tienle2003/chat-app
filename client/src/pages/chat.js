694e2423c5dbdc82f5da5ed67a43f2ed70b50049
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2024-02-01 22:57:25 +0700

    add component

--- client/src/pages/chat.js
+++ client/src/pages/chat.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Sidebar from "../components/sidebar";
+import Content from "../components/content";
 
 const Chat = () => {
   return (
-    <div className="d-flex">
+    <div className="chat-container">
       <Sidebar />
+      <Content />
     </div>
   );
 };
