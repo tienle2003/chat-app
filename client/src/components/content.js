695b6581f2c5506df7d7ce590ff164985461ab45
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2024-02-01 22:58:16 +0700

    initial content page

---
+++ client/src/components/content.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import "./content.css";
+
+function Content() {
+    return(
+        <>
+            <div className="content">
+                This is the content
+            </div>
+        </>
+    );
+}
+
+export default Content;
