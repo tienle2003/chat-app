c51b614250f41aacf9833bd2be46de7eea17095f
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2024-02-25 14:18:23 +0700

    init loginLogoStyle

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,72 +1,85 @@
-import React from "react";
-import "./login.css";
+import React, { useState } from "react";
+import "./css/common.css";
+import "./css/login.css";
 import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  const navigate = useNavigate();
-  const handleSignIn = () => {
-    navigate("/register");
+  const loginLogoStyle = {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
   };
 
+  const navigate = useNavigate();
+
   return (
-    <div className="login-page">
-      <form className="login-form">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          width="120"
-          height="120"
-        >
-          <path fill="none" d="M0 0h24v24H0z" />
-          <path
-            d="M4.929 19.071A9.969 9.969 0 0 1 2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10H2l2.929-2.929zM11 6v12h2V6h-2zM7 9v6h2V9H7zm8 0v6h2V9h-2z"
-            fill="rgba(114,105,239,1)"
-          />
-        </svg>
-        <h1>Chat App</h1>
-        <div className="input-box">
-          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
-            <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z" />
-          </svg>
-          <input className="for-username" type="text" id="username" name="username" placeholder="Username" />
+    <div className="login-page my-5 pt-sm-5">
+      <div className="container">
+        <div className="justify-content-center row">
+          <div className="col-md-8 col-lg-6 col-xl-5">
+            <div className="text-center mb-4">
+              <a className="auth-logo mb-5 d-block" href="#">
+                <div style={loginLogoStyle}>
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="30" height="30"><path fill="none" d="M0 0h24v24H0z" /><path d="M4.929 19.071A9.969 9.969 0 0 1 2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10H2l2.929-2.929zM11 6v12h2V6h-2zM7 9v6h2V9H7zm8 0v6h2V9h-2z" fill="rgba(114,105,239,1)" /></svg>
+                  <span className="logo-text">Chatvia</span>
+                </div>
+              </a>
+              <h4>Sign in</h4>
+              <p className="text-muted mb-4">
+                Sign in to continue to Chatvia.
+              </p>
+            </div>
+            <div className="card">
+              <div className="p-4 card-body">
+                <div className="p-3">
+                  <form className="">
+                    <div className="mb-3">
+                      <label className="form-label form-label">Username</label>
+                      <div className="mb-3 bg-soft-light rounded-3 input-group">
+                        <span className="input-group-text text-muted" id="basic-addon-3">
+                          <i class="fa-regular fa-user"></i>
+                        </span>
+                        <input id="email" name="email" placeholder="Enter email" type="text" className="form-control form-control-lg border-light bg-soft-light is-invalid form-control" ></input>
+                        <div className="invalid-feedback" type="invalid">Please Enter Your Username</div>
+                      </div>
+                    </div>
+                    <div className="mb-4 mb-3">
+                      <div className="float-end">
+                        <a href="/forget-password" className="text-muted font-size-13">Forgot password?</a>
+                      </div>
+                      <label className="form-label form-label">Password</label>
+                      <div className="mb-3 bg-soft-light rounded-3 input-group">
+                        <span className="input-group-text text-muted">
+                          <i class="fa-solid fa-lock"></i>
+                        </span>
+                        <input type="password" id="password" name="password" placeholder="Enter password" className="form-control form-control-lg border-light bg-soft-light is-invalid form-control" ></input>
+                        <div className="invalid-feedback" type="invalid">Please Enter Your Password</div>
+                      </div>
+                    </div>
+                    <div className="form-check mb-4">
+                      <input id="remember-check" type="checkbox" className="form-check-input form-check-input"></input>
+                      <label for="remember-check" className="form-check-label form-label" id="remember-checkbox">Remember me</label>
+                    </div>
+                    <div className="d-grid">
+                      <button type="submit" className=" waves-effect waves-light btn btn-primary d-block w-100">Sign in</button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+            <div className="mt-5 text-center">
+              <p>
+                Don't have an account ?
+                <a href="/register" className="font-weight-medium text-primary"> Register now</a>
+              </p>
+              <p>
+                <i class="ic-copyright fa-regular fa-copyright"></i>
+                2024 Chatvia. Crafted with <i class="ic-heart fa-solid fa-heart"></i> by Themesbrand
+              </p>
+            </div>
+          </div>
         </div>
-        <div className="input-box">
-          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-shield-lock" viewBox="0 0 16 16">
-            <path d="M5.338 1.59a61 61 0 0 0-2.837.856.48.48 0 0 0-.328.39c-.554 4.157.726 7.19 2.253 9.188a10.7 10.7 0 0 0 2.287 2.233c.346.244.652.42.893.533q.18.085.293.118a1 1 0 0 0 .101.025 1 1 0 0 0 .1-.025q.114-.034.294-.118c.24-.113.547-.29.893-.533a10.7 10.7 0 0 0 2.287-2.233c1.527-1.997 2.807-5.031 2.253-9.188a.48.48 0 0 0-.328-.39c-.651-.213-1.75-.56-2.837-.855C9.552 1.29 8.531 1.067 8 1.067c-.53 0-1.552.223-2.662.524zM5.072.56C6.157.265 7.31 0 8 0s1.843.265 2.928.56c1.11.3 2.229.655 2.887.87a1.54 1.54 0 0 1 1.044 1.262c.596 4.477-.787 7.795-2.465 9.99a11.8 11.8 0 0 1-2.517 2.453 7 7 0 0 1-1.048.625c-.28.132-.581.24-.829.24s-.548-.108-.829-.24a7 7 0 0 1-1.048-.625 11.8 11.8 0 0 1-2.517-2.453C1.928 10.487.545 7.169 1.141 2.692A1.54 1.54 0 0 1 2.185 1.43 63 63 0 0 1 5.072.56" />
-            <path d="M9.5 6.5a1.5 1.5 0 0 1-1 1.415l.385 1.99a.5.5 0 0 1-.491.595h-.788a.5.5 0 0 1-.49-.595l.384-1.99a1.5 1.5 0 1 1 2-1.415" />
-          </svg>
-          <input className="for-password" type="password" id="password" name="password" placeholder="Password" />
-        </div>
-        <div className="detail-instruction">
-          <span className="forgot-password">
-            <a href="#">Forgot password?</a>
-          </span>
-        </div>
-        <div className="btn-groups">
-          <button type="button" onClick={handleSignIn}>Sign up</button>
-          <button type="submit">Login</button>
-        </div>
-        <div className="other-login">
-          <hr></hr>
-          <span>Or login with</span>
-          <hr></hr>
-        </div>
-        <div className="social-login">
-          <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
-            <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951" />
-          </svg>
-          <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="currentColor" class="bi bi-twitter-x" viewBox="0 0 16 16">
-            <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z" />
-          </svg>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="45" height="45">
-            <path fill="#4285F4" d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" />
-            <path fill="#34A853" d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" />
-            <path fill="#FBBC05" d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z" />
-            <path fill="#EA4335" d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" />
-            <path fill="none" d="M1 1h22v22H1z" />
-          </svg>
-        </div>
-      </form>
+      </div>
     </div>
   );
 };
