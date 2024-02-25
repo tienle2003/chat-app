6ff0e2bd1b00158eaeb7850da0f420aeb1d3f658
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2024-02-25 14:20:00 +0700

    init js Profiles

---
+++ client/src/pages/components/profile.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Chat from "../chat";
+
+const Profile = () => {
+    return (
+        <Chat>
+            <h2>Profile Page</h2>
+        </Chat>
+    );
+};
+export default Profile;
