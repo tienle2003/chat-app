637477800d7eea2eca6ed678bd77482a0ab2b225
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2024-02-25 14:16:26 +0700

    useState from react

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Routes, Route, Navigate, BrowserRouter } from "react-router-dom";
 
 import "./App.css";
@@ -7,18 +7,24 @@ import Login from "./pages/login";
 import Register from "./pages/register";
 import Sidebar from "./components/sidebar";
 import Content from "./components/content";
+import Chats from "./pages/components/chats";
+import Profile from "./pages/components/profile";
+import Groups from "./pages/components/groups";
+import Settings from "./pages/components/settings";
+import ChatGPT from "./pages/components/chatGPT";
 
 function App() {
+  const [selectedContent, setSelectedContent] = useState("");
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Chat />}></Route>
-          <Route path="/chats" element={<Chat />}></Route>
-          <Route path="/group" element={<Chat />}></Route>
-          <Route path="/chat-gpt" element={<Chat />}></Route>
-          <Route path="/profile" element={<Chat />}></Route>
-          <Route path="/setting" element={<Chat />}></Route>
+          <Route path="/chats" element={<Chats />}></Route>
+          <Route path="/group" element={<Groups />}></Route>
+          <Route path="/chat-gpt" element={<ChatGPT />}></Route>
+          <Route path="/profile" element={<Profile />}></Route>
+          <Route path="/setting" element={<Settings />}></Route>
           <Route path="/login" element={<Login />}></Route>
           <Route path="/register" element={<Register />}></Route>
           <Route path="*" element={<Navigate to="/" />}>
