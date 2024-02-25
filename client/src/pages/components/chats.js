1f0fd5d99123e357647d12d4292d3900c796ca52
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2024-02-25 14:19:42 +0700

    init js Chats

---
+++ client/src/pages/components/chats.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Chat from "../chat";
+
+const Chats = () => {
+    return (
+        <Chat>
+            <h2>Chats Page</h2>
+            
+        </Chat>
+    );
+};
+export default Chats;
