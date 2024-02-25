6042bad19f3aadc2ce83106037def05a49dd2b96
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2024-02-25 14:19:51 +0700

    init js Groups

---
+++ client/src/pages/components/groups.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Chat from "../chat";
+
+const Groups = () => {
+    return (
+        <Chat>
+            <h2>Groups Page</h2>
+        </Chat>
+    );
+};
+export default Groups;
