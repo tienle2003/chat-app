e835101df10cc1c9d339395fdf2d66379532da74
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2024-02-27 01:02:16 +0700

    content

--- client/src/components/content.js
+++ client/src/components/content.js
@@ -1,13 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
 import "./content.css";
+import Chats from "../pages/components/chats"
+import ChatGPT from "../pages/components/chatGPT"
+import Groups from "../pages/components/groups"
+import Profile from "../pages/components/profile"
+import Settings from "../pages/components/settings"
 
-const Content = ({ children }) => {
+const Content = () => {
     return (
-        <>
-            <div className="content">
-                {children}
+        <div className="content chat-leftsidebar me-lg-1">
+            <div className="tab-content">
+                
             </div>
-        </>
+        </div>
     );
 };
 
