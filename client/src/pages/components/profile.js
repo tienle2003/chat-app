635e03577a4f556575fc8d3d8948c8f14e29cd05
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2024-02-27 01:03:22 +0700

    component Profile

--- client/src/pages/components/profile.js
+++ client/src/pages/components/profile.js
@@ -3,9 +3,10 @@ import Chat from "../chat";
 
 const Profile = () => {
     return (
-        <Chat>
-            <h2>Profile Page</h2>
-        </Chat>
+        // <Chat>
+        //     <h2>Profile Page</h2>
+        // </Chat>
+        <h2>Profile Page</h2>
     );
 };
 export default Profile;
