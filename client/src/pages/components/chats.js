3f0bdff2ae4ab84213f73160316a8c57498290ae
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2024-02-27 01:02:55 +0700

    component Chat

--- client/src/pages/components/chats.js
+++ client/src/pages/components/chats.js
@@ -3,10 +3,10 @@ import Chat from "../chat";
 
 const Chats = () => {
     return (
-        <Chat>
-            <h2>Chats Page</h2>
-            
-        </Chat>
+        // <Chat>
+        //     <h2>Chats Page</h2>
+        // </Chat>
+        <h2>Chats Page</h2>
     );
 };
 export default Chats;
