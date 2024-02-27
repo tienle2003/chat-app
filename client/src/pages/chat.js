8f3b90a7705521463dff0d94adad29f00599fb90
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2024-02-27 17:55:48 +0700

    import {useState}

--- client/src/pages/chat.js
+++ client/src/pages/chat.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Sidebar from "../components/sidebar";
 import Content from "../components/content";
 import UserChats from "../components/user-chat";
