50335c702868f1a2b07fe12c0c3c95fe900901d3
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2024-02-28 18:33:19 +0700

    add components of page

--- client/src/components/content.js
+++ client/src/components/content.js
@@ -1,16 +1,22 @@
 import React from "react";
 import "./content.css";
-import Chats from "../pages/components/chats"
-import ChatGPT from "../pages/components/chatGPT"
-import Groups from "../pages/components/groups"
-import Profile from "../pages/components/profile"
-import Settings from "../pages/components/settings"
+import Profile from "../pages/components/profile";
+import Chats from "../pages/components/chats";
+import Groups from "../pages/components/groups";
+import Contact from "../pages/components/contacts";
+import Settings from "../pages/components/settings";
+import ChatGPT from "../pages/components/chatGPT";
 
 const Content = () => {
     return (
         <div className="content chat-leftsidebar me-lg-1">
             <div className="tab-content">
-                
+                <Profile></Profile>
+                <Chats></Chats>
+                <Groups></Groups>
+                <Contact></Contact>
+                <Settings></Settings>
+                <ChatGPT></ChatGPT>
             </div>
         </div>
     );
